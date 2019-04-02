6d87fc307f57bb548082cd1d59800b4d06e3c912
Sam Saks-Fithian <samsaksfithian@gmail.com>
2019-04-02 12:54:17 -0700

    delete in progress

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
     };
   }
 
+  // TODO: add version that returns the top level boss also
+  // TODO: make this recursively search tree structure
+  // TODO: add abilitiy to choose what tree to search
   findEmployeeByName = name => this.state.employees.find(emp => emp.name === name);
 
   handleAddEmployee = event => {
@@ -50,10 +53,39 @@ class App extends Component {
     event.target.reportsTo.value = '';
   };
 
-  // handleDeleteEmployee = event => {
-  //   event.preventDefault();
-  //   const name =
-  // }
+  handleDeleteEmployee = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+
+    const employee = this.findEmployeeByName(name);
+    if (!employee) {
+      window.alert('Invalid employee name');
+      return;
+    }
+
+    const topBoss = {}; // will get this from modified findEmployeeByName
+    const topCopy = { ...topBoss };
+
+    const boss = this.findEmployeeByName(employee.reportsTo); // TODO: make this search in topCopy
+
+    // cut out the employee from the boss' underlings array
+    const delEmpArr = boss.underlings.splice(
+      boss.underlings.findIndex(emp => emp.name === employee.name),
+      1,
+    );
+    // add all the underlings of the deleted employee to the underlings of the boss
+    delEmpArr[0].underlings.forEach(emp => {
+      boss.underlings.push(emp);
+      emp.reportsTo = boss.name;
+    });
+
+    this.setState(prevState => {
+      const index = prevState.employees.findIndex(emp => emp.name === topCopy.name);
+      return {
+        employees: [...prevState.slice(0, index), topCopy, ...prevState.slice(index + 1)],
+      };
+    });
+  };
 
   render() {
     console.log('Employees: ', this.state.employees);
@@ -64,10 +96,10 @@ class App extends Component {
           <input type="text" name="reportsTo" placeholder="Who do they report to" />
           <button type="submit">Add Employee</button>
         </form>
-        {/* <form onSubmit={this.handleDeleteEmployee}>
+        <form onSubmit={this.handleDeleteEmployee}>
           <input type="text" name="name" placeholder="Employee name" />
           <button type="submit">Delete Employee</button>
-        </form> */}
+        </form>
         <EmployeeList employees={this.state.employees} />
       </div>
     );
